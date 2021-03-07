d3d88f3f3611043c34ddd3d44dbecdd1d1121407
Siddharth Telang <stelang@umd.edu>
2021-03-07 15:09:42 -0500

    [ENPM_661][Project 2] : Add main function to input the start and end points and find the path

--- Project2/path.py
+++ Project2/path.py
@@ -108,6 +108,7 @@ def isInObstacleSpace(i,j):
 
     return 0
 
+# function to find the moves from given coordinates
 def find_moves(i,j, obj):
     moves = ['N','NE', 'E', 'SE', 'S', 'SW','W', 'NW']
     final_moves = ['N','NE', 'E', 'SE', 'S', 'SW','W', 'NW']
@@ -118,3 +119,96 @@ def find_moves(i,j, obj):
             final_moves.remove(moves[move])
     #print(final_moves)
     return final_moves
+
+
+def printDebugLines(mainList, backTrack):
+    print(mainList)
+    print('---------------------------------------------------------------------\n\n')
+    for i in range(len(backTrack)):
+        print('--------------------------------------------------------------------')
+        print(backTrack[i].state)
+        print(backTrack[i].parent_index)
+        print(backTrack[i].parent_state)
+        print('---------------------------------------------------------------------\n')
+
+def main():
+    while(True):
+        print('Enter the initial start point one by one - x: 0-399 ; y - 0-299')
+        initial_state = [int(input()), int(input())]
+        print('Enter the Goal point one by one - x: 0-399 ; y - 0-299')
+        goal_state = [int(input()), int(input())]
+        if (isInObstacleSpace(initial_state[0], initial_state[1])):
+            print('Initial state is in obstacle space. Please select another start point')
+            continue
+        elif (isInObstacleSpace(goal_state[0], goal_state[1])):
+            print('Goal state is in obstacle space. Please select another final point')
+            continue
+        elif (initial_state == goal_state):
+            print('Initial and goal state are the same. Select different initial and goal states')
+        else:
+            break
+
+    # create a main queue to insert nodes and deque them
+    mainQueue = Queue()
+    # create a parent node and insert in queue
+    parent = Node(initial_state, 0, '')
+    mainQueue.enque(parent)
+
+    # create main list to store all visited nodes, and back tracking list
+    mainList, backTrack = [initial_state], []
+
+    # define a list of moves
+    movesList = ['N','NE', 'E', 'SE', 'S', 'SW','W', 'NW']
+
+    # define flag to set if goal reached and a count for number of expansions
+    flag, count = 0, 0
+
+    print('Finding path...............................................')
+
+    while(mainQueue.not_empty()):
+        # deque object
+        obj = mainQueue.deque()
+        # coordinates to expand
+        to_expand = obj.state
+        if count == 0:
+            backTrack.append(obj)
+        count += 1
+
+        # find the available moves for the coordinates
+        i, j = to_expand[0], to_expand[1]
+        moves = find_moves(i,j, obj)
+
+        # dictionary to save the move value
+        moves_i = {'N':i, 'NE':i+1, 'E':i+1, 'SE':i+1, 'S':i, 'SW':i-1, 'W':i-1, 'NW':i-1}
+        moves_j = {'N':j+1, 'NE':j+1, 'E':j, 'SE':j-1, 'S':j-1, 'SW':j-1, 'W':j, 'NW':j+1}
+
+        # iterate through each availabe move, generate it using dictionary value and store in node
+        for move in moves:
+            new_state = [moves_i.get(move), moves_j.get(move)]
+            # save the parent info : count = parent index ; parent_state = parent coordinates
+            child = Node(new_state, count, move)
+            child.parent_state = to_expand
+
+            # if coordinates are not visited, append and enque
+            if (child.state not in mainList):
+                mainList.append(child.state)
+                backTrack.append(child)
+                print(new_state)
+                mainQueue.enque(child)
+
+            # if goal state is reached, break out of the loop
+            if (child.state == goal_state):
+                print('Goal reached')
+                flag = 1
+                break
+
+        if (flag == 1):
+            break
+
+    print('Goal state reached... Back tracking now')
+    #printDebugLines(mainList, backTrack)
+
+
+if __name__ == '__main__':
+    main()
+    exit()
