ab4c66d52b38ce0466eda8e908b3de2cc555b3b5
Siddharth Telang <stelang@umd.edu>
2021-03-07 12:40:37 -0500

    [ENPM_661][Project 2] : Add Node class to store parent and child info ; Add Queue class

--- Project2/path.py
+++ Project2/path.py
@@ -12,6 +12,48 @@ import matplotlib.pyplot as plt
 import math
 import cv2
 
+# define a Queue class for storing the nodes to be expanded
+class Queue:
+
+    # init function
+    def __init__(self):
+        self.queue = []
+
+    # insert in queue
+    def enque(self, item):
+        self.queue.insert(0, item)
+
+    # pop node
+    def deque(self):
+        if self.queue:
+            return self.queue.pop()
+        return 'Empty'
+
+    # check if queue is not empty
+    def not_empty(self):
+        return (len(self.queue) > 0)
+
+    # check if the queue contains a specific list item
+    def contains(self, item):
+        return (item in self.queue)
+
+    # print the queue
+    def __print__(self):
+        print(self.queue)
+
+# define a class Node for storing all nodes generated in map with parent node info
+class Node:
+
+    # init function to store info about:
+    # child: own state and move
+    # parent: index, state and move
+    def __init__(self, state, parent_index, move):
+        self.state = state # child list
+        self.move = move
+        self.parent_index = parent_index # parent index
+        self.parent_state = [] # parent list/state form
+        self.parent_move = ''
+
 # function to check if the move is in obstacle space
 def isInObstacleSpace(i,j):
 
